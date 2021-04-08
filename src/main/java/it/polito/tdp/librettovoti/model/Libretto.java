19d016f8df4a060e0b30ece5f93e40cc83b45578
utente <utente@pcnb-01>
2021-04-08 14:33:15 +0200

    soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -13,8 +13,30 @@ public class Libretto {
 		this.voti.add(v);
 		
 	}
+	public List<Voto> listaVotiUguali(int punteggio){
+	//creo una lista nuova ed aggiungo i voti uguali
+	//ricorda che l'oggetto v è lo stessso che appartiene
+	//alle 2 liste diverse, perchèsino a che non faccio new
+	//v rappresenta quell'oggetto.	
+		List<Voto> risultato =  new ArrayList<>();
+		for(Voto v : this.voti) {
+			if(v.getVoto() == punteggio) {
+				risultato.add(v);
+			}		
+		}
+		return risultato;
+	}
+	public Libretto votiUguali (int punteggio) {
+		Libretto risultato = new Libretto();
+		for(Voto v: this.voti) {
+			if (v.getVoto()==punteggio){
+				risultato.add(v);
+			}
+		}
+	return risultato;
+	}
 	/**
-	 * Il toString() del libretto costruisca a mano, 
+	 * Il toString() della classe Libretto costruisce a mano, 
 	 * possiamo iterare sulla lista con for e chiedo
 	 * a v di stampare il singolo voto e lo metto a capo
 	 */
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 	public static void main (String[] args) {
@@ -8,10 +9,15 @@ public class TestLibretto {
 	Libretto libretto = new Libretto();
 	Voto voto1 = new Voto("Analisi-1", 25, LocalDate.of(2020, 3, 12));
 	libretto.add(voto1);
+	libretto.add(new Voto("Chimica", 25, LocalDate.of(2020, 7, 15)));
 	libretto.add(new Voto("Fisica-1", 30, LocalDate.of(2020, 7, 15)));
 	libretto.add(new Voto("Informatica", 24, LocalDate.of(2020, 12, 15)));
 	
-	System.out.println(libretto);// se qui faccio run mi stampa il reference dell'oggetto libretto, mi stampa la memoria dove quello oggetto vivee non il libretto.
+	System.out.println(libretto);// se qui faccio run mi stampa il reference dell'oggetto libretto, mi stampa la memoria dove quello oggetto vive, e non il libretto.
+	List<Voto> venticinque = libretto.listaVotiUguali(25);
+	System.out.println(venticinque);
+	Libretto librettoventicinque = libretto.votiUguali(25);
+	System.out.println(librettoventicinque);
 	}
 	
 }
